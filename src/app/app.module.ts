87c1c172689e54f62b7160ade673a7f48609e3df
jdferreira <jdferreira@fundacionparaguaya.org.py>
2019-01-17 14:11:56 -0300

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// rutas
+import { APP_ROUTES } from './app.routes';
+
+// modulos
+import { PageModule } from './pages/pages.module';
+
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES,
+    PageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
